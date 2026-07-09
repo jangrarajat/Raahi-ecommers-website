52f8afae1b5a9df63a4ddd86e33ea3f629d124fb
jangrarajat <rajatjangra940@gmail.com>
2026-07-09 23:35:15 +0530

    adding first dist file

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(cors({
 app.use(express.json())   // config json
 connectDB()  // connect data ba
 
+app.use(express.static('public')); // Serve static files from the "public" directory
 
 
 app.get('/', (req, res) => {
